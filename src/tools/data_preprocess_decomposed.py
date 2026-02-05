0928229f18817e3cacdb8f982e1446bcdb470625
JaeYoung Seon <42963882+tjswodud@users.noreply.github.com>
2026-02-05 16:53:10 +0900

    fixed typo

--- src/tools/data_preprocess_decomposed.py
+++ src/tools/data_preprocess_decomposed.py
@@ -112,7 +112,7 @@ def main(args):
         data_dir = "./data/HumanML3D/"
         min_motion_length = 40
     elif args.dataset == "kit":
-        data_dir = "./data/KIT-ML-Condition/"
+        data_dir = "./data/KIT-ML/"
         min_motion_length = 24
     else:
         raise ValueError(f"{args.dataset} not supported!")
